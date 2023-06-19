974fe0e5d0200a6fb4bd4ee7d2966d805e0347f6
Daniel Rajmon <daniel@rajmon.com>
2023-06-19 20:49:07 +0200

    facade-use-case

---
+++ src/facade/use-case/users.ts
@@ -0,0 +1,45 @@
+import Reports from './reports';
+import Wallets from './wallets';
+
+export default class Users {
+  static instance: Users;
+  #users: { [id: string]: { [id: string]: string } } = {};
+  #reports = new Reports();
+  #wallets = new Wallets();
+
+  constructor() {
+    if (Users.instance) {
+      return Users.instance;
+    }
+    Users.instance = this;
+  }
+
+  registerUser(newUser: { [id: string]: string }): string {
+    if (!(newUser['user_name'] in this.#users)) {
+      // generate really complicated unique user_id
+      // using the existing user_name as the id for simplicity
+      const userId = newUser['user_name'];
+      this.#users[userId] = newUser;
+      this.#reports.logEvent(`new user '${userId}' created`);
+      this.#wallets.createWallet(userId);
+      this.#reports.logEvent(`Give new user '${userId}' sign up bonus of 10`);
+      this.#wallets.adjustBalance(userId, 10);
+      return userId;
+    }
+    return '';
+  }
+
+  editUser(userId: string, user: { [id: string]: string }): boolean {
+    // not implemented
+    console.log(userId);
+    console.log(user);
+    return false;
+  }
+
+  changePwd(userId: string, password: string): boolean {
+    // not implemented
+    console.log(userId);
+    console.log(password);
+    return false;
+  }
+}
